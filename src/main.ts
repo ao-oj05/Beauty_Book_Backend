6d4486d864f582b27c110682ee2f987e5faaca9d
ao-oj05 <aliciaocana49@gmail.com>
2026-04-22 14:40:25 -0600

    fix: increase payload size limit to 50mb

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { urlencoded, json } from 'express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -7,6 +8,8 @@ async function bootstrap() {
     origin: '*', // Permitir que el frontend se conecte sin problemas
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
   await app.listen(process.env.PORT || 3001, '0.0.0.0');
 }
 bootstrap();
